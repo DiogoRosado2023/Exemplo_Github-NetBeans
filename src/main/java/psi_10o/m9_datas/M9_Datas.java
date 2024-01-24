d0377dcd83c63818b0b554188996f23c95089a8b
a22908 <a22908@d1-08-03.dped.local>
2024-01-24 10:30:51 +0000

    Exemplo de atualização do repositorio

--- src/main/java/psi_10o/m9_datas/M9_Datas.java
+++ src/main/java/psi_10o/m9_datas/M9_Datas.java
@@ -4,7 +4,8 @@ public class M9_Datas {
 
     public static void main(String[] args) {
         Data mes1 = new Data(1,12,2032);
-        
+        //inicialização do repositorio
+        //para exemplo de primeira vez
         mes1.setAno(2023);
         mes1.setDia(07);
         mes1.setMes(02);
