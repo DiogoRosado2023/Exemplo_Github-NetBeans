ecd22bbc18ff7ae5c1ed2e3e5645047c6c68fecb
a22908 <a22908@d1-08-03.dped.local>
2024-01-24 10:27:29 +0000

    Exemplo de commit 1. Atualizar repositorio local.

---
+++ src/main/java/psi_10o/m9_datas/M9_Datas.java
@@ -0,0 +1,15 @@
+package psi_10o.m9_datas;
+
+public class M9_Datas {
+
+    public static void main(String[] args) {
+        Data mes1 = new Data(1,12,2032);
+        
+        mes1.setAno(2023);
+        mes1.setDia(07);
+        mes1.setMes(02);
+        
+        mes1.verDataNumerica();
+        mes1.verDataTexto();
+    }
+}
